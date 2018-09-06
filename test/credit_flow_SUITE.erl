77286ec29de9e1b01606bc32052bf1a2fefa7799
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2018-09-06 15:37:26 +0200

    Add acking_by_the_receiver_unblocks_sender/1 test

--- test/credit_flow_SUITE.erl
+++ test/credit_flow_SUITE.erl
@@ -8,7 +8,8 @@
 %%
 
 all() ->
-    [running_out_of_credit_blocks_sender].
+    [running_out_of_credit_blocks_sender,
+     acking_by_the_receiver_unblocks_sender].
 
 init_per_suite(Config) ->
     set_default_credit(#{initial => 3, more => 1}),
@@ -22,25 +23,59 @@ end_per_suite(Config) ->
 %%
 
 running_out_of_credit_blocks_sender(_) ->
+    %% given
     N = 3,
     Receiver = spawn_link(?MODULE, receiver_loop, [N]),
+    %% when
     [ begin
           credit_flow:send(Receiver)
       end || _ <- lists:seq(1, N) ],
+    %% then
     ?assert(credit_flow:blocked()).
+
+acking_by_the_receiver_unblocks_sender(_) ->
+    %% given
+    N = 3,
+    Receiver = spawn_link(?MODULE, receiver_loop, [N + 1, fun ack/1]),
+    %% when
+    [ begin
+          credit_flow:send(Receiver)
+      end || _ <- lists:seq(1, N) ],
+    %% then
+    ?assert(credit_flow:blocked()),
+    %% when
+    Receiver ! {ack_please, self()},
+    receive
+        {bump_credit, Credit} ->
+            credit_flow:handle_bump_msg(Credit)
+        after timer:seconds(1) ->
+            ct:fail(sender_timeout)
+    end,
+    %% then
+    ?assert(not credit_flow:blocked()).
 
 %%.
 %%' Helpers
 %%
 
-receiver_loop(0) -> ok;
-receiver_loop(N) ->
+receiver_loop(N) -> receiver_loop(N, fun no_op/1).
+
+receiver_loop(0, _) -> ok;
+receiver_loop(N, HandleMessage) ->
     receive
-        _M ->
-            receiver_loop(N-1)
+        M ->
+            HandleMessage(M),
+            receiver_loop(N-1, HandleMessage)
         after timer:seconds(1) ->
             ct:fail(receiver_timeout)
     end.
+
+no_op(_) -> ok.
+
+ack({ack_please, Pid}) ->
+    credit_flow:ack(Pid);
+ack(_) ->
+    ok.
 
 set_default_credit(#{initial := Initial, more := More}) ->
     application:load(credit_flow),
